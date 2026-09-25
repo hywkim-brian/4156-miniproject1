37c0cbd82b9f531afe3a1cf2f85d21474a4a598e
Hyunwoo Kim <hyunwook@Hyunwoos-MacBook-Pro.local>
2026-09-25 13:10:17 -0400

    Add update price request model

---
+++ IndividualProject/src/main/java/com/taxapi/model/UpdatePriceRequest.java
@@ -0,0 +1,31 @@
+package com.taxapi.model;
+
+/**
+ * A request to update an item's price.
+ */
+public final class UpdatePriceRequest {
+
+    /** new base price. */
+    private Double basePrice;
+
+    public UpdatePriceRequest() {
+    }
+
+    /**
+     * Gets the new base price.
+     *
+     * @return the new base price
+     */
+    public Double getBasePrice() {
+        return basePrice;
+    }
+
+    /**
+     * Sets the new base price.
+     *
+     * @param basePrice the new base price
+     */
+    public void setBasePrice(final Double basePrice) {
+        this.basePrice = basePrice;
+    }
+}
